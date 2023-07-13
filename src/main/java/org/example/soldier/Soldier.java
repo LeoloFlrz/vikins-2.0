22b1ec894f745fd7592a5447952fcd1f6603fab2
LeoloFlrz <leoloflrzr@gmail.com>
2023-07-13 09:54:35 +0200

    completed soldier tests

--- src/main/java/org/example/soldier/Soldier.java
+++ src/main/java/org/example/soldier/Soldier.java
@@ -1,4 +1,19 @@
 package org.example.soldier;
 
 public class Soldier {
+    int health;
+    int strength;
+
+    public Soldier(int health, int strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    public int attack() {
+        return this.strength;
+    }
+
+    public int receiveDamage(int damage) {
+        return this.health -= damage;
+    }
 }
