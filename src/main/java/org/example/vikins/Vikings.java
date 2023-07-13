038dbf234989272b36e8eb62625e7df5a076cf4d
LeoloFlrz <leoloflrzr@gmail.com>
2023-07-13 12:56:14 +0200

    completed some tests

--- src/main/java/org/example/vikins/Vikings.java
+++ src/main/java/org/example/vikins/Vikings.java
@@ -1,4 +1,34 @@
 package org.example.vikins;
 
-public class Vikings {
+import org.example.soldier.Soldier;
+
+public class Vikings extends Soldier {
+    String name;
+    int health;
+    int strength;
+    String msg;
+
+    public Vikings(String name, int health, int strength) {
+        super(health, strength);
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+
+    @Override
+    public int attack() {
+        return this.strength;
+    }
+
+    public int receiveDamage(int damage) {
+        return this.health -= damage;
+    }
+
+    public String getMsg() {
+        if (this.health <= 0) {
+            return this.msg = this.name + " has died in combat";
+        } else {
+            return this.msg = this.name + " has received "+ this.strength + " points of damage";
+        }
+    }
 }
